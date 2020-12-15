5990972cc517ff7e4731426ee33a5c5152069d23
humorous-storage-7521 <rmwerner@umich.edu>
2020-12-14 19:25:01 -0600

    reverseint

--- exercises/reverseint/index.js
+++ exercises/reverseint/index.js
@@ -8,6 +8,12 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    let str = n.toString().split('').reduce((rev, char) => char + rev, '')
+    if (Math.sign(n) < 0) {
+        str = '-' + str.substr(0, str.length-1)
+    }
+    return parseInt(str)
+}
 
 module.exports = reverseInt;
