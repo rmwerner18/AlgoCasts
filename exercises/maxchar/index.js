0ee66756ba235b81ffd43b781561c77ca3b2d829
humorous-storage-7521 <rmwerner@umich.edu>
2020-12-14 20:07:37 -0600

    maxchar

--- exercises/maxchar/index.js
+++ exercises/maxchar/index.js
@@ -5,6 +5,12 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    let obj = {}
+    for (let i = 0; i < str.length; i++) {
+        obj[str[i]] = obj[str[i]] + 1 || 1
+    }
+    return Object.keys(obj).reduce((a, b) => obj[a] > obj[b] ? a : b)
+}
 
 module.exports = maxChar;
