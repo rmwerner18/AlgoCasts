ae02d5a5d9c3d44da6a0f16928d53e8fb41903b4
humorous-storage-7521 <rmwerner@umich.edu>
2020-12-15 17:28:19 -0600

    chunk

--- exercises/chunk/index.js
+++ exercises/chunk/index.js
@@ -8,6 +8,15 @@
 // chunk([1, 2, 3, 4, 5], 4) --> [[ 1, 2, 3, 4], [5]]
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
-function chunk(array, size) {}
+function chunk(array, size) {
+    let newArray = []
+    let i = 0
+    while (i < array.length) {
+        let arr = array.slice(i, i + size)
+        newArray.push(arr)
+        i = i + size
+    }
+    return newArray
+}
 
 module.exports = chunk;
