2fa4e4c9794d4dd9f7881dd2d14ed044be6a5709
humorous-storage-7521 <rmwerner@umich.edu>
2020-12-22 10:56:06 -1000

    pyramid

--- exercises/pyramid/index.js
+++ exercises/pyramid/index.js
@@ -14,6 +14,29 @@
 //       ' ### '
 //       '#####'
 
-function pyramid(n) {}
+function pyramid(n, row = 0, stair = '') {
+    let l = n*2 - 1
+    let midpoint = Math.floor(l/2)
+    if (row === n) {
+        return
+    } else if (stair.length === l) {
+        console.log(stair)
+        return pyramid(n, row + 1)
+    }
+    let add = (stair.length <= midpoint + row && stair.length >= midpoint - row) ? "#" : " "
+    pyramid(n, row, stair + add)
+} 
 
 module.exports = pyramid;
+
+// for (let i = 1; i <= n; i++) {
+//     let stair  = ''
+//     for (let j = 1; j < n*2; j++) {
+//         let hashes = i*2 - 1
+//         let spaces = (n*2-hashes)/2
+//         if (j <= spaces || j >= spaces+hashes) {
+//             stair += " "
+//         } else stair += "#"
+//     }
+//     console.log(stair)
+// }
