aaf0fa30b41668b73020f5beb5afe2ef9fb6dbfd
humorous-storage-7521 <rmwerner@umich.edu>
2020-12-22 10:21:33 -1000

    capitalize and steps

--- exercises/steps/index.js
+++ exercises/steps/index.js
@@ -17,6 +17,45 @@
 //       '### '
 //       '####'
 
-function steps(n) {}
+function steps(n, row = 0, stair = '') {
+    if (row === n) {
+        return
+    } else if (stair.length === n) {
+        console.log(stair)
+        return steps(n, row+1)
+    } else if (stair.length <= row) {
+        stair += "#"
+    } else {
+        stair += " "
+    }
+    steps(n, row, stair)
+}
 
 module.exports = steps;
+
+// for (let i = 0; i < n; i++) {
+//     let stair = ''
+//     for (let j = 0; j < n; j++) {
+//         if (j <= i) {
+//             stair += '#'
+//         } else {
+//             stair += " "
+//         }
+//     }
+//     console.log(stair)
+// }
+
+
+// let outerCount = 0
+// while (outerCount < n) {
+//     let str = ''
+//     let innerCount = 0
+//     while (innerCount < n) {
+//         if (innerCount <= outerCount) {
+//             str = '#' + str
+//         } else str = str + " "
+//         innerCount += 1
+//     }
+//     console.log(str)
+//     outerCount += 1
+// }
